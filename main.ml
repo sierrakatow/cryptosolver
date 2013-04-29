b565d0a7897d02802d717f676a503d17ae55b171
sam <spottash@college.harvard.edu>
2013-04-28 22:07:41 -0400

    commented and completed main.ml

--- main.ml
+++ main.ml
@@ -3,11 +3,21 @@ open Crypto
 
 (* Takes in a cryptogram and returns answers. *)
 let main (c: Crypto.cryptogram) : answers = 
+  
+  (* Loads dictionary into tree *)
   let dict_lst = Load.read_file pos.txt in
   let dict_tree = 
     List.map (fun x y -> Dictionary.insert x y) Leaf dict_lst in
+
+  (* Breaks cyptogram into a list of cwords *)
   let cwords = Str.split (Str.regexp "[ \t]+") c in
+
+  (* Retrieves possible words from the dictionary to match with cwords *)
   let choices = 
-    List.map (fun s -> Dictionary.lookup (To_scheme.to_schme s)) cwords in
-  List.map (fun x -> ) Decide.decide choices cwords
+    List.map (fun s -> Dictionary.lookup (To_scheme.to_scheme s)) cwords in
+
+  (* Condenses the best choices into an answer list. *)
+  let print_answer (choices : choice list) = 
+    List.fold_right (fun x -> x.word ^ " " ^ y.word) lst ""
+  List.map print_answer (Decide.decide choices cwords)
 ;;
